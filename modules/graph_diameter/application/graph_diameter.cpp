5825a223e3cf19ca63334c2f8bde17620c7af720
Kazhaeva <a.kazhaeva7@mail.ru>
2022-05-18 14:37:57 +0300

    commit 1

---
+++ modules/graph_diameter/application/graph_diameter.cpp
@@ -0,0 +1,14 @@
+// Copyright 2022 Kazhaeva Anastasia
+
+#include <iostream>
+#include <string>
+
+#include "include/graph_diameter_app.h"
+
+
+int main(int argc, const char** argv) {
+	GraphApplication app;
+	std::string result = app(argc, argv);
+	std::cout << result << std::endl;
+	return 0;
+}
